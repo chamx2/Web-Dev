028e85f0f4776138fdfd604977a142dfca9b618b
John Hay Supetran <superjohnhay@gmail.com>
2020-09-11 04:53:49 +0800

    Javascript time!

---
+++ js/learn-to-code.js
@@ -0,0 +1,30 @@
+// var name = "John";
+// var age = "26";
+
+// var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+// var firstName = "John";
+// var lastName = "Super";
+// var dateOfBirth = "10-10-10";
+// var age = 26;
+// var profileImgUrl = "http://coolpicks.com/johnjacob.jpg"
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd Birthday!";
+
+
+// //alert(message);
+// console.log(loginWelcomeMessage);
+
+var total = 10;
+
+for(var x = 0; x < total; x++ )
+{
+    console.log(x);
+}
+
+var students = ["John", "Jacob", "Jingle", "Heimer", "Vanessa"];
+
+for(var x = 0; x < students.length; x++)
+{
+    console.log(students[x]);
+}
